f8a4e3d4cd611e1fc801429019564eb7752a3ff9
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 18:16:40 +0200

    add css reset a livello global

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import Logo from './Logo.jsx';
 import Navbar from './Navbar.jsx';
+import style from './Header.module.css'
 
 function Header() {
-    return <header> 
+    return <header className={`d-flex ${style.header}`}> 
         <Logo/>
         <Navbar/>
     </header> 
