aa1dd59e21c5c1132ca60657b3be0987526cc017
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:11:09 +0200

    add: import di FooterAsideNavigation in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import FooterAsideNavigation from "./FooterAsideNavigation";
+
 function Footer() {
     return <footer>
         <h1>Sono il footer</h1>
+        <FooterAsideNavigation/>
     </footer>
 }
 
