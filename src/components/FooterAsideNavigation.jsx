e36900a7b1ab4724a1e19d2a72efe4b8e45904ab
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:10:43 +0200

    add: list item in FooterAsideNavigation.jsx

---
+++ src/components/FooterAsideNavigation.jsx
@@ -0,0 +1,45 @@
+function FooterAsideNavigation() {
+    return <div className={"footer-aside-container"}>
+        <ul>
+            <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+             <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+             <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+             <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+             <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+             <li>
+                <div className={"aside-list-item"}>
+                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
+                    <p> Digital Comics</p>
+                </div>
+            </li>
+
+        </ul>
+    </div>
+}
+
+export default FooterAsideNavigation;
