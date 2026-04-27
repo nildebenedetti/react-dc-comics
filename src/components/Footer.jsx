58765330c756fca5c05cd9b46a49666990cd8680
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:32:58 +0200

    add: import di FooterLinkTrees.jsx in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import FooterAsideNavigation from "./FooterAsideNavigation";
+import FooterLinkTrees from "./FooterLinkTrees";
 
 function Footer() {
     return <footer>
         <h1>Sono il footer</h1>
         <FooterAsideNavigation/>
+        <FooterLinkTrees/>
     </footer>
 }
 
