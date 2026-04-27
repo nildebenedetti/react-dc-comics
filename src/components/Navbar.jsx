dd38a5b72d17c165c871056c326d5dec9880c2e3
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 18:41:36 +0200

    add: css dedicato per modulo navbar in navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+import style from './Navbar.module.css'
+
 function Navbar() {
-     return <> <ul>
+     return <> <ul className={`d-flex ${style.ul}`}>
         <li>Characters</li>
         <li>Comics</li>
         <li>Movies</li>
