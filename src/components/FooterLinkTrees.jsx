fa5755e1e4bcaf5131aca2e53b2d202120decb34
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:33:27 +0200

    add: 3 linktree div su 5 in FooterLinkTree.jsx

---
+++ src/components/FooterLinkTrees.jsx
@@ -0,0 +1,92 @@
+
+function FooterLinkTrees() {
+    return <div className={"footer-linktrees-container"}>
+
+        <div className={"footer-link-tree"}>
+            <h3>DC COMICS</h3>
+            <ul>
+                <li>
+                    <p>Characters</p>
+                </li>
+                <li>
+                    <p>Comics</p>
+                </li>
+                <li>
+                    <p>Movies</p>
+                </li>
+                <li>
+                    <p>TV</p>
+                </li>
+                <li>
+                    <p>Games</p>
+                </li>
+                <li>
+                    <p>Videos</p>
+                </li>
+                <li>
+                    <p>News</p>
+                </li>
+            </ul>
+        </div>
+        <div className={"footer-link-tree"}>
+            <h3>DC</h3>
+            <ul>
+                <li>
+                    <p>Terms Of Use</p>
+                </li>
+                <li>
+                    <p>Privacy Policy (new)</p>
+                </li>
+                <li>
+                    <p>Ad Choices</p>
+                </li>
+                <li>
+                    <p>Advertising</p>
+                </li>
+                <li>
+                    <p>Jobs</p>
+                </li>
+                <li>
+                    <p>Subscriptions</p>
+                </li>
+                <li>
+                    <p>Talent Workshops</p>
+                </li>
+                <li>
+                    <p>CPSC Certificates</p>
+                </li>
+                <li>
+                    <p>Ratings</p>
+                </li>
+                <li>
+                    <p>Shop Help</p>
+                </li>
+                <li>
+                    <p>Contact Us</p>
+                </li>
+            </ul>
+        </div>
+        <div className={"footer-link-tree"}>
+            <h3>SITES</h3>
+            <ul>
+                <li>
+                    <p>DC</p>
+                </li>
+                <li>
+                    <p>MAD Magazine</p>
+                </li>
+                <li>
+                    <p>DC Kids</p>
+                </li>
+                <li>
+                    <p>DC Universe</p>
+                </li>
+                <li>
+                    <p>DC Power Visa</p>
+                </li>
+            </ul>
+        </div>
+    </div>;
+}
+
+export default FooterLinkTrees;
