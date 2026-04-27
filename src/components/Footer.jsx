3e3070c27b3e3dff0f28bda81d9653ff88b0cdc3
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:46:31 +0200

    add: social component to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import FooterAsideNavigation from "./FooterAsideNavigation";
 import FooterLinkTrees from "./FooterLinkTrees";
+import FooterSocialNCTA from "./FooterSocialNCTA";
 
 function Footer() {
     return <footer>
         <h1>Sono il footer</h1>
         <FooterAsideNavigation/>
         <FooterLinkTrees/>
+        <FooterSocialNCTA/>
     </footer>
 }
 
