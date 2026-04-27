fc35e4ca244faa3b5fec3fa3fcfd1d9c3d9311dc
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 22:54:23 +0200

    add: style specifico per aside bar in footer.css

--- src/components/FooterAsideNavigation.jsx
+++ src/components/FooterAsideNavigation.jsx
@@ -1,33 +1,35 @@
+import style from './FooterAsideNavigation.module.css';
+
 function FooterAsideNavigation() {
-    return <div className={"footer-aside-container"}>
-        <ul>
+    return <div className={"footer-aside-container bg-blue fluid"}>
+        <ul className={`d-flex ${style.asideList}`}>
             <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="dc digital comics icon"></img>
+                <div className={`d-flex ${style.asideComponent}`}>
+                    <img className={style.asideImg} src="/img/buy-comics-digital-comics.png" alt="dc digital comics icon"></img>
                     <p>DIGITAL COMICS</p>
                 </div>
             </li>
              <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-merchandise.png" alt="dc merchandise icon"></img>
+                <div className={`d-flex ${style.asideComponent}`}>
+                    <img className={style.asideImg} src="/img/buy-comics-merchandise.png" alt="dc merchandise icon"></img>
                     <p>DC MERCHANDISE</p>
                 </div>
             </li>
              <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-subscriptions.png" alt="dc subscriptions icon"></img>
+               <div className={`d-flex ${style.asideComponent}`}>
+                    <img className={style.asideImg} src="/img/buy-comics-subscriptions.png" alt="dc subscriptions icon"></img>
                     <p>SUBSCRIPTION</p>
                 </div>
             </li>
              <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-shop-locator.png" alt="dc comic shop locator icon"></img>
+                <div className={`d-flex ${style.asideComponent}`}>
+                    <img className={style.asideLocatorImg} src="/img/buy-comics-shop-locator.png" alt="dc comic shop locator icon"></img>
                     <p>COMIC SHOP LOCATOR</p>
                 </div>
             </li>
              <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-dc-power-visa.svg" alt=" dc digital power visa icon"></img>
+                <div className={`d-flex ${style.asideComponent}`}>
+                    <img className={style.asideImg} src="/img/buy-dc-power-visa.svg" alt=" dc digital power visa icon"></img>
                     <p>DC POWER VISA</p>
                 </div>
             </li>
