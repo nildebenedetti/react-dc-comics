2e684ac9f4ece91379305820686b308dc2d57a3a
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:46:11 +0200

    add: import to sign-up btn in FooterSOcialNCTA.jsx

---
+++ src/components/FooterSocialNCTA.jsx
@@ -0,0 +1,9 @@
+import FooterSignUpBtn from "./FooterSignUpBtn";
+
+function FooterSocialNCTA() {
+    return <div className={"footer-social-n-cta-container"}>
+        <FooterSignUpBtn/>
+    </div>
+}
+
+export default FooterSocialNCTA;
