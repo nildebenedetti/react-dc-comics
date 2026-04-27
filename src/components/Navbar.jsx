d42c9b245ed361746eb6964406b48958c108d577
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 16:44:50 +0200

    add: ul navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+     return <> <ul>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Collectibles</li>
+        <li>Videos</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+    </ul>
+    </>
+}
+
+export default Navbar
