e45f6e6bde70469586d5a108701cf17c9701863d
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:57:07 +0200

    add: content in function di FooterSocialHub.jsx

---
+++ src/components/FooterSocialHub.jsx
@@ -0,0 +1,38 @@
+function FooterSocialHub() {
+    return <>
+    <div className={"social-elem-container"}>
+        <h3 className={"follow-cta"}>FOLLOW US</h3>
+        <div className={"social-icons-container"}>
+            <ul>
+                <li>
+                    <div className={"single-logo-container"}>
+                        <img src="/img/footer-facebook.png" alt="facebook logo"></img>
+                    </div>
+                </li>
+                <li>
+                    <div className={"single-logo-container"}>
+                        <img src="/img/footer-twitter.png" alt="twitter logo"></img>
+                    </div>
+                </li>
+                <li>
+                    <div className={"single-logo-container"}>
+                        <img src="/img/footer-youtube.png" alt="youtube logo"></img>
+                    </div>
+                </li>
+                <li>
+                    <div className={"single-logo-container"}>
+                        <img src="/img/footer-pinterest.png" alt="pinterest logo"></img>
+                    </div>
+                </li>
+                <li>
+                    <div className={"single-logo-container"}>
+                        <img src="/img/footer-periscope.png" alt="periscope icon"></img>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </>
+}
+
+export default FooterSocialHub;
