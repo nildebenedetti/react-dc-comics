ba74cc3ce41de2285a080bcf7ce11087889d6c9f
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 17:17:25 +0200

    fix: adjusted list items content in FooterAsideNavigation.jsx

--- src/components/FooterAsideNavigation.jsx
+++ src/components/FooterAsideNavigation.jsx
@@ -3,41 +3,34 @@ function FooterAsideNavigation() {
         <ul>
             <li>
                 <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
+                    <img src="/img/buy-comics-digital-comics.png" alt="dc digital comics icon"></img>
+                    <p>DIGITAL COMICS</p>
                 </div>
             </li>
              <li>
                 <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
+                    <img src="/img/buy-comics-merchandise.png" alt="dc merchandise icon"></img>
+                    <p>DC MERCHANDISE</p>
                 </div>
             </li>
              <li>
                 <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
+                    <img src="/img/buy-comics-subscriptions.png" alt="dc subscriptions icon"></img>
+                    <p>SUBSCRIPTION</p>
                 </div>
             </li>
              <li>
                 <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
+                    <img src="/img/buy-comics-shop-locator.png" alt="dc comic shop locator icon"></img>
+                    <p>COMIC SHOP LOCATOR</p>
                 </div>
             </li>
              <li>
                 <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
+                    <img src="/img/buy-dc-power-visa.svg" alt=" dc digital power visa icon"></img>
+                    <p>DC POWER VISA</p>
                 </div>
             </li>
-             <li>
-                <div className={"aside-list-item"}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="icona digital comics"></img>
-                    <p> Digital Comics</p>
-                </div>
-            </li>
-
         </ul>
     </div>
 }
