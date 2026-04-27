f7beef96dec0b21ae0928d70fe9fca64ac7c28fa
“Nilde <nilde.benedetti@gmail.com>
2026-04-27 22:35:24 +0200

    add: footer style e import in footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import FooterAsideNavigation from "./FooterAsideNavigation";
 import FooterLinkTrees from "./FooterLinkTrees";
 import FooterSocialNCTA from "./FooterSocialNCTA";
+import style from './Footer.module.css';
 
 function Footer() {
-    return <footer  className={"mx-auto d-flex"}>
+    return <footer  className={`mx-auto d-flex flex-column ${style.footer}`}>
         <FooterAsideNavigation/>
         <FooterLinkTrees/>
         <FooterSocialNCTA/>
