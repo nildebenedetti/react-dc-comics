3b0105d3b585b6cf683956bce9b5ac37d3b45d6d
“Nilde <nilde.benedetti@gmail.com>
2026-04-28 16:59:36 +0200

    add: function renderCard in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,23 @@
 import { comics } from '../data/comics.js';
+
+{/* Aggiungo la funzione per fare render da dati oggetto */}
+
+function renderCards(objList) {
+    let myCardsList = objList.map( objEl => {
+        const { id, series, thumb} = objEl;
+
+        return <li key={id}>
+            <div className={style.imgContainer}>
+                <img className={style.img} src={thumb} alt={`cover of ${series}`}></img>
+                <p className={style.titlePar}>
+                    {series.toUpperCase()}
+                </p>
+            </div>
+        </li>
+    });
+}
+
+
 function Main() {
     return <main className={"mx-auto d-flex"}>
         <h1>Sono il main</h1>
