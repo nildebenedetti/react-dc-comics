1b0a2888857cf14b94f3121d37d1f7784544ac21
“Nilde <nilde.benedetti@gmail.com>
2026-04-28 15:10:46 +0200

    add: array di oggetti per prop

--- src/components/FooterSocialHub.jsx
+++ src/components/FooterSocialHub.jsx
@@ -1,6 +1,34 @@
 import style from './FooterSocialHub.module.css';
 
-function FooterSocialHub() {
+const socialElList = [
+    {
+        id: 1,
+        title: 'Facebook',
+        src: '/img/footer-facebook.png'
+    },
+    {
+        id: 2,
+        title: 'Twitter',
+        src: '/img/footer-twitter.png'
+    },
+    {
+        id: 3,
+        title: 'Youtube',
+        src: '/img/footer-youtube.pn'
+    },
+    {
+        id: 4,
+        title: 'Pinterest',
+        src: '/img/footer-pinterest.png'
+    },
+    {
+        id: 5,
+        title: 'Periscope',
+        src: '/img/footer-periscope.png'
+    }
+];
+
+{/* function FooterSocialHub() {
     return <>
     <div className={`${style.followCta} d-flex`}>
         <h3 className={style.cta}>FOLLOW US</h3>
@@ -35,6 +63,7 @@ function FooterSocialHub() {
         </div>
     </div>
     </>
-}
+}*/}
 
 export default FooterSocialHub;
+ 
