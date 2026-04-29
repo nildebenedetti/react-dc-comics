6e4f99b35a8a9f80e2d1121994ff363fa7dc2b20
“Nilde <nilde.benedetti@gmail.com>
2026-04-29 16:22:42 +0200

    fix: titolo a capo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,18 @@
 import comics from '../data/comics.js';
 import style from './Main.module.css'
 
-{/* Aggiungo la funzione per fare render da dati oggetto*/}
+{/* Aggiungo la funzione per fare render da dati oggetto*/ }
 
 function renderCards(objList) {
-    let myCardsList = objList.map( objEl => {
-        const { id, series, thumb} = objEl;
+    let myCardsList = objList.map(objEl => {
+        const { id, series, thumb } = objEl;
 
         return <li key={id}>
             <div className={style.imgContainer}>
                 <img className={style.img} src={thumb} alt={`cover of ${series}`}></img>
-                <p className={style.titlePar}>
-                    {series.toUpperCase()}
-                </p>
+                    <p className={style.titlePar}>
+                        {series.toUpperCase()}
+                    </p>
             </div>
         </li>
     });
@@ -23,19 +23,19 @@ function renderCards(objList) {
 
 function Main() {
     return <main className={`mx-auto fluid ${style.container}`}>
-            <div className="hero-banner">
+        <div className="hero-banner">
+        </div>
+        <div className="label-wrapper">
+            <div className="label-easy">
+                <p>CURRENT SERIES</p>
             </div>
-            <div className="label-wrapper">
-                <div className="label-easy">
-                    <p>CURRENT SERIES</p>
-                </div>
-            </div>
-            <ul className={`mx-auto ${style.row}`}>
-                {renderCards(comics)}
-            </ul>
-            <div className="btn-wrapper">
-                <button className="btn-easy">LOAD MORE</button>
-            </div>
+        </div>
+        <ul className={`mx-auto ${style.row}`}>
+            {renderCards(comics)}
+        </ul>
+        <div className="btn-wrapper">
+            <button className="btn-easy">LOAD MORE</button>
+        </div>
     </main>
 }
 
